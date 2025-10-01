4854e08f987fb492ef2d740d5af0375f56b0f222
OWOEOMO <50674595+OWOEOMO@users.noreply.github.com>
2025-10-02 07:27:40 +0800

    Add Fancy UI

--- S381F-Project-inventory/server.js
+++ S381F-Project-inventory/server.js
@@ -16,6 +16,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
+app.use(require('express').static('public'));
 app.use(cookieSession({
   name: 'session',
   keys: ['secretKeyHere'],
